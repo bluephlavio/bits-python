825ca4a0434d7c8502d289515e8c8865cede9809
Flavio Grandin <flavio.grandin@gmail.com>
2024-09-30 15:50:29 +0000

    ci: trying to debug TemplateNotFound error

--- tests/e2e/test_cli.py
+++ tests/e2e/test_cli.py
@@ -17,8 +17,17 @@ def test_cli_help():
     )
 
 
-def test_cli_build(bitsfiles):
+def test_cli_build(bitsfiles, capfd):
     for bitsfile in bitsfiles:
+
+        print(f"Testing file: {bitsfile}")
+        with open(bitsfile, "r", encoding="utf-8") as file:
+            print(file.read())
+
+        # Capture the output
+        captured = capfd.readouterr()
+        print(captured.out)
+
         result = subprocess.run(
             ["bits", "build", str(bitsfile)], capture_output=True, check=True
         )
