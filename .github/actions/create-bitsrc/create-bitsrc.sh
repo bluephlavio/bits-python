c4e176d51284631f780efc7a6b8d16940f2b922c
Flavio Grandin <flavio.grandin@gmail.com>
2024-09-30 15:16:07 +0000

    ci: added debug echo statements to create-bitsrc.sh file

--- .github/actions/create-bitsrc/create-bitsrc.sh
+++ .github/actions/create-bitsrc/create-bitsrc.sh
@@ -3,9 +3,15 @@
 # Resolve the current working directory
 WORKSPACE_DIR=${GITHUB_WORKSPACE}
 
+# Debug: Print the value of GITHUB_WORKSPACE
+echo "GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}"
+
 # Create the .bitsrc file with environment-specific variables
 cat <<EOL > .bitsrc
 [variables]
 templates = ${WORKSPACE_DIR}/tests/resources/templates
 artifacts = ${WORKSPACE_DIR}/tests/artifacts
 EOL
+
+# Debug: Print the contents of the .bitsrc file
+cat .bitsrc
